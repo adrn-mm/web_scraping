4881f393737da956f3d79c48aa8c4df31a12a241
adrn-mm <adrian.m.muhammad@gmail.com>
2023-07-02 21:01:30 +0700

    first commit

---
+++ scraping_tokopedia.py
@@ -0,0 +1,72 @@
+# Import libraries
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+import pandas as pd
+import time
+from selenium.common.exceptions import NoSuchElementException
+
+# Create an empty list to store the data
+data = []
+
+# Configure selenium
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=options)
+
+# Loop through pages 1 to 10
+for page in range(1, 11):
+    # Construct the page URL
+    url = f"https://www.tokopedia.com/search?navsource=&ob=5&page={page}&q=laptop&srp_component_id=04.06.00.00&srp_page_id=&srp_page_title=&st="
+
+    driver.get(url)
+
+    time.sleep(10)
+
+    # Scraping process
+    products = driver.find_elements(By.XPATH, "//div[@class='prd_link-product-name css-3um8ox']")
+
+    for product in products:
+        product_name = product.text
+
+        parent = product.find_element(By.XPATH, "..")
+
+        try:
+            price_element = parent.find_element(By.XPATH, ".//div[@class='prd_link-product-price css-1ksb19c']")
+            price = price_element.text
+        except NoSuchElementException:
+            price = None
+
+        try:
+            location_element = parent.find_element(By.XPATH, ".//span[@class='prd_link-shop-loc css-1kdc32b flip']")
+            location = location_element.text
+        except NoSuchElementException:
+            location = None
+
+        try:
+            rating_element = parent.find_element(By.XPATH, ".//span[@class='prd_rating-average-text css-t70v7i']")
+            rating = rating_element.text
+        except NoSuchElementException:
+            rating = None
+
+        try:
+            sales_element = parent.find_element(By.XPATH, ".//span[@class='prd_label-integrity css-1duhs3e']")
+            sales = sales_element.text
+        except NoSuchElementException:
+            sales = None
+
+        data.append({
+            'Product': product_name,
+            'Price': price,
+            'Rating': rating,
+            'Location': location,
+            'Sales': sales,
+        })
+
+driver.quit()
+
+# Create a DataFrame from the collected data
+df = pd.DataFrame(data)
+
+# Create a CSV file
+filepath = "web_scraping/scraped_tokopedia_data.csv"
+df.to_csv(filepath, index=False)
---
+++ scraping_website_BI.py
@@ -0,0 +1,51 @@
+"""
+
+//*[@id="ctl00_PlaceHolderMain_g_6c89d4ad_107f_437d_bd54_8fda17b556bf_ctl00_GridView1"]/table
+
+
+"""
+
+
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Configure selenium
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=options)
+
+# Navigate to the website
+driver.get("https://www.bi.go.id/id/statistik/informasi-kurs/transaksi-bi/Default.aspx")
+
+# Wait for the table to be visible
+table = driver.find_element(By.XPATH, "//*[@id="ctl00_PlaceHolderMain_g_6c89d4ad_107f_437d_bd54_8fda17b556bf_ctl00_GridView1"]/table")
+
+# Get the HTML content of the table
+table_html = table.get_attribute("innerHTML")
+
+# Close the browser
+driver.quit()
+
+# Process the table HTML content using Beautiful Soup or other methods
+# Example: Use Beautiful Soup to parse the table
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(table_html, "html.parser")
+
+# Find all table rows
+rows = soup.find_all("tr")
+
+# Extract data from the table rows and store in a list of lists
+data = []
+for row in rows:
+    # Get data from table cells
+    cells = row.find_all("td")
+    row_data = [cell.text for cell in cells]
+    data.append(row_data)
+
+# Write the data to a CSV file
+with open("output.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(data)
+
+print("Data saved to output.csv")
